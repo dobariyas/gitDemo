e40874d3d561cd46736e15d149556b15950b1d94
Sandip Dobariya <sandipdobairya93@gmail.com>
2023-06-12 12:53:07 -0400

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//TestNG XML file from Maven
+	
+	
+	@Test
+	public void BrowserAutomation() {
+		
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementUI() {
+		
+		System.out.println("ElementUI");
+	}
+}
